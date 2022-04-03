bf2c3431fa9725c6f3df01c0219b1b420ed65591
yjz <491356290@qq.com>
2022-04-03 22:46:29 +0800

    fix some errors

--- src/main/java/assign1/birthDate.java
+++ src/main/java/assign1/birthDate.java
@@ -1,6 +1,9 @@
 package assign1;
 
 import java.io.*;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -18,7 +21,17 @@ public class birthDate {
         if (args.length == 2){
             start = Integer.valueOf(args[0]);
             end = Integer.valueOf(args[1]);
+            File file = new File("heap.4096");
+            startTime = System.currentTimeMillis();
+            readFile(file);
+            endTime = System.currentTimeMillis();
+            stdout();
         }
+       /*File file = new File("assign1/heap.txt");
+       start = 19700101;
+       end = 20020101;
+       readFile(file);
+       stdout();*/
     }
 
     public static void readFile(File file){
@@ -29,21 +42,27 @@ public class birthDate {
             fis = new FileInputStream(file);
             ois = new ObjectInputStream(fis);
             while (true){
-                page = (Page)ois.readObject();
-                pages.add(page);
-                birthdayQuery();
-                pages.remove(page);
+                try {
+                    page = (Page)ois.readObject();
+                    pages.add(page);
+                    birthdayQuery();
+                    pages.remove(page);
+                } catch (EOFException e){
+                    break;
+                }
+
             }
 
         } catch (IOException e) {
             e.printStackTrace();
+
         } catch (ClassNotFoundException e) {
             System.out.println("Class not found!");
             e.printStackTrace();
         } finally {
             try {
-                ois.close();
                 fis.close();
+                ois.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -54,11 +73,11 @@ public class birthDate {
     public static void birthdayQuery(){
         for (Page page : pages){
             for (Artist artist : page.artists){
-                if (artist.getBirthDate()!=null){
-                    if (Integer.valueOf(artist.getBirthDate())>=start && Integer.valueOf(artist.getBirthDate())<=end){
-                        matchedRecords.add(artist.getBirthDate());
-                        numOfArtists++;
-                    }
+                int day = dateFormatProcess(artist.getBirthDate());
+                if (day>=start && day<=end){
+                    String result = artist.getPersonName()+": "+day;
+                    matchedRecords.add(result);
+                    numOfArtists++;
                 }
             }
         }
@@ -72,5 +91,46 @@ public class birthDate {
         }
         long timeTaken = startTime - endTime;
         System.out.println("Total time taken is "+timeTaken);
+    }
+
+    public static int dateFormatProcess(String birthdayDate){
+        if (birthdayDate.contains("\"")){
+            String str = "";
+            for (int i = 0; i < birthdayDate.length(); i++) {
+                if (birthdayDate.charAt(i) != '"'){
+                    str += birthdayDate.charAt(i);
+                }
+            }
+            birthdayDate = str;
+        }
+        if (!birthdayDate.contains("{")){
+            if (birthdayDate.contains("T")){
+                int indexOfT = birthdayDate.indexOf("T");
+                birthdayDate = birthdayDate.substring(0,indexOfT);
+            }
+        }else {
+            int index = birthdayDate.indexOf("|");
+            birthdayDate = birthdayDate.substring(1,index);
+            if (birthdayDate.contains("T")){
+                int indexOfT = birthdayDate.indexOf("T");
+                birthdayDate = birthdayDate.substring(1,indexOfT);
+            }
+        }
+        if (birthdayDate.contains("/")){
+            birthdayDate.replace('/','-');
+        }
+        DateFormat df1 = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = new Date();
+        try {
+            date = df1.parse(birthdayDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        DateFormat df2 = new SimpleDateFormat("yyyyMMdd");
+        String date2 = df2.format(date);
+        int intBirthDay = Integer.valueOf(date2);
+
+        return intBirthDay;
+
     }
 }
